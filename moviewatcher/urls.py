fa4fcf63051b43d447862a14df7cb7359524cbb0
Eliminator746 <90308790+Eliminator746@users.noreply.github.com>
2025-09-18 09:07:43 +0530

    Initial commit - Django project

---
+++ moviewatcher/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+
+# Donot write imports here when you're using include
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('movie/', include('watchlist_app.urls'))
+]
